b684ca7c4fcf532eb11ebb8030582d8fe8d6eec0
Kostas Petrianakis <kpetrianakis@gmail.com>
2012-10-16 01:16:40 +0300

    Git ignore, sln etc

---
+++ NAsanaTests/WorkspaceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NAsanaTests
+{
+    [TestClass]
+    public class WorkspaceTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestNewWorkspace()
+        {
+        }
+    }
+}
